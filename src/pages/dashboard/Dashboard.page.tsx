2599ac1a02cbdd98731f331a8543c7b20fc42ad1
CarlosMansoCid <carlosmanso12@gmail.com>
2023-10-02 11:37:59 -0400

    added basic metadata at all the pages

--- src/pages/dashboard/Dashboard.page.tsx
+++ src/pages/dashboard/Dashboard.page.tsx
@@ -1,10 +1,14 @@
 import DashboardLayout from "../../layouts/dashboardLayout/Dashboard.layout"
-
+import {Helmet} from "react-helmet";
 
 const Dashboard = () => {
   
   return (
     <DashboardLayout>
+      <Helmet>
+        <title>Slack | Dashboard</title>
+        <meta name="description" content="Bienvenidos a slack"/>
+      </Helmet>
         body content
     </DashboardLayout>
   )
