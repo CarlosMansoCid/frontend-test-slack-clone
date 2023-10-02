fce90d80f63ebc1247074a6032b6f313a76e72fd
CarlosMansoCid <carlosmanso12@gmail.com>
2023-10-02 10:19:10 -0400

    added the responsive desing to login and signup pages

--- src/pages/LoginPage/LoginPage.page.tsx
+++ src/pages/LoginPage/LoginPage.page.tsx
@@ -4,6 +4,7 @@ import OAuthButtonsContainer from "../../modules/OAuthButtonsContainer/OAuthButt
 import AuthFormsContainer from "../../components/AuthFormsContainer/AuthFormsContainer"
 import AuthSeparator from "../../modules/AuthHeader/components/AuthSeparator"
 import LoginForm from "../../modules/LoginForm/LoginForm"
+import { styled } from "styled-components"
 
 const LoginPage = () => {
   return (
@@ -11,12 +12,28 @@ const LoginPage = () => {
         <AuthHeader title='Inicia sesión en Slack' 
                     subtitle={`Te sugerimos que uses la dirección de correo electrónico que usas en el trabajo.`}/>
         <AuthFormsContainer>
+          <FormContainer>
           <OAuthButtonsContainer/>
           <AuthSeparator/>
-          <LoginForm/>
+            <LoginForm/>
+          </FormContainer>
         </AuthFormsContainer>
     </LoginLayout>
   )
 }
 
 export default LoginPage
+
+const FormContainer = styled.div`
+  width: 30vw;
+
+  @media (width < 900px){
+    width: 60vw;
+  }
+  @media (width < 700px){
+    width: 80vw;
+  }
+  @media (width < 500px){
+    width: 96vw;
+  }
+`
